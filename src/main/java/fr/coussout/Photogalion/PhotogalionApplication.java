244e8f761552fc8f343b22a9a85ea4d833d7927f
CocoAll <corentin.all@gmail.com>
2021-09-25 14:48:54 +0200

    chore(init function): removed config for the database rewrite on each launch and data initialisation

--- src/main/java/fr/coussout/Photogalion/PhotogalionApplication.java
+++ src/main/java/fr/coussout/Photogalion/PhotogalionApplication.java
@@ -1,27 +1,15 @@
 package fr.coussout.Photogalion;
 
-import fr.coussout.Photogalion.service.PhotoGalionServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class PhotogalionApplication implements CommandLineRunner {
-	@Autowired
-	private PhotoGalionServiceImpl photogalionInitService;
-
+public class PhotogalionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PhotogalionApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		photogalionInitService.initCategory();
-		photogalionInitService.initMember();
 	}
 
 }
--- src/main/java/fr/coussout/Photogalion/service/IPhotogalionService.java
+++
@@ -1,6 +0,0 @@
-package fr.coussout.Photogalion.service;
-
-public interface IPhotogalionService {
-	public void initMember();
-	public void initCategory();
-}
